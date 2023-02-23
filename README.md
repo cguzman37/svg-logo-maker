f843cdd5671d50e7effb5e820855a4fe779dc9e1
Cynthia Guzman <cynthiaguzman@Cynthias-MacBook-Air-2.local>
2023-02-22 22:49:33 -0600

    added all the shape classes and got the code to work

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # svg-logo-maker
+
+# Description:
+This application makes it easy for users to create a quick and simple svg logo just by answering a few prompt questions.
+# Installation:
+User will need to install inquirier in order to run this application.
+In the terminal do npm i inquirer@8.2.4 and it will downloaded the version needed.
+Also user must install jest in order to test the application.
+
+# User Story:
+AS a freelance web developer
+I WANT to generate a simple logo for my projects
+SO THAT I don't have to pay a graphic designer
+
+![This is an image](/example/logo.svg)
+
+# Walkthrough link:
+
+# Acknowledgements:
+
+The testing code was provided by our teacher assissant. I also worked in a study group with multiple students.
---
+++ index.js
@@ -0,0 +1,58 @@
+const inquirer = require('inquirer');
+const {writeFile} = require('fs/promises');
+const {Circle, Triangle, Square } = require('./lib/shapes');
+const Svg = require("./lib/svg");
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo:',
+        validate: function(values){
+            if (values.length > 3 ){
+                return 'Text must be less than or equal to three characters';
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (keyword or color#):'
+    },
+    {
+        type: 'list',
+        name: 'shapeType',
+        message: 'Select a shape:',
+        choices: ['Circle', 'Triangle', 'Square']
+
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (keyword or color#):'
+    }
+    
+];
+inquirer
+.prompt(questions)
+.then(({text,textColor, shapeType, shapeColor})=>{
+    let shape;
+    switch (shapeType){
+        case 'Circle':
+            shape = new Circle();
+            break;
+        case 'Triangle':
+            shape= new Triangle();
+            break;
+        default: 
+        shape = new Square();
+        break;
+    }
+    shape.setColor(shapeColor)
+    const svg = new Svg()
+    svg.setText(text, textColor)
+    svg.setShape(shape)
+    return writeFile("./example/logo.svg", svg.render())
+})
+.then(()=> console.log("Generated logo.svg"))
+.catch(err => console.log(err));
